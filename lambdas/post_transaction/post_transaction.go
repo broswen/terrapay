b09ab4d2b6b41eb72b28ba8ae6113efb2d99ae0f
Brad <broswen@gmail.com>
2021-09-05 23:13:34 -0500

    add post transaction lambda

--- lambdas/post_transaction/post_transaction.go
+++ lambdas/post_transaction/post_transaction.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/broswen/terrapay/account"
 )
+
+var ddbClient *dynamodb.Client
+var accountService *account.AccountService
 
 type PostTransactionRequest struct {
 	Recipient string  `json:"recipient"`
@@ -14,14 +23,50 @@ type PostTransactionRequest struct {
 }
 
 type PostTransactionResponse struct {
-	Transaction account.Transaction
+	Transactions account.Transaction
 }
 
 func HandleRequest(ctx context.Context, event events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 
-	// get info from source
-	// validate destination exists, get info
+	authorization, ok := event.Headers["Authorization"]
+	if !ok {
+		log.Println("missing jwt")
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: 401,
+		}, nil
+	}
 
+	authorizationParts := strings.Split(authorization, " ")
+	if len(authorizationParts) != 2 {
+		log.Println("invalid parts for bearer authorization")
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: 401,
+		}, nil
+	}
+
+	claims, err := account.ValidateJWT(authorizationParts[1])
+	if err != nil {
+		log.Println(err.Error())
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: 401,
+		}, nil
+	}
+
+	var request PostTransactionRequest
+	err = json.Unmarshal([]byte(event.Body), &request)
+	if err != nil {
+		return events.APIGatewayV2HTTPResponse{}, err
+	}
+
+	fmt.Printf("%v\n", request)
+
+	transaction, err := accountService.PostTransaction(ctx, claims.Subject, request.Recipient, request.Amount)
+	if err != nil {
+		log.Println(err.Error())
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: 500,
+		}, nil
+	}
 	// validate source has amount
 
 	// submit write transaction with 4 records
@@ -32,9 +77,34 @@ func HandleRequest(ctx context.Context, event events.APIGatewayV2HTTPRequest) (e
 
 	// return transaction id
 
+	response := PostTransactionResponse{
+		Transactions: transaction,
+	}
+
+	j, err := json.Marshal(response)
+	if err != nil {
+		log.Println(err.Error())
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: 500,
+		}, nil
+	}
+
 	return events.APIGatewayV2HTTPResponse{
 		StatusCode: 200,
+		Body:       string(j),
 	}, nil
+}
+
+func init() {
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	ddbClient = dynamodb.NewFromConfig(cfg)
+	accountService, err = account.NewFromClient(ddbClient)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
